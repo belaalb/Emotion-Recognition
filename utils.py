b9aec68df56f9b55c7b6e01366842f269c5cc3c8
Isabela Albuquerque <isabelamcalbuquerque@gmail.com>
2017-07-18 12:20:36 -0400

    Finished processing for one subject

--- utils.py
+++ utils.py
@@ -24,19 +24,32 @@ def split_data_per_subject(segment_duration = 1, sampling_rate = 128, sub = 1, m
 	number_of_trials = data.shape[0]
 	number_of_segments = data.shape[2]/number_of_samples
 
+	number_of_final_examples = number_of_trials*number_of_segments
+
 	final_data = []
 	final_labels = []
 
 	for trial in xrange(0, number_of_trials):
+
 		data_to_split = data[trial, :, :]
 		data_to_split = np.reshape(data_to_split, (number_of_segments, data.shape[1], number_of_samples))
 		final_data.append(data_to_split)
 
-	return final_data, final_labels		
+		label_to_repeat = labels[trial, :]
+		label_repeated = np.tile(label_to_repeat, (number_of_segments, 1))
+		final_labels.append(label_repeated)
+
+	final_data_out = np.reshape(np.asarray(final_data), (number_of_final_examples, data.shape[1], number_of_samples))
+	final_labels_out = np.reshape(np.asarray(final_labels), (number_of_final_examples, labels.shape[1]))	
+
+	return final_data_out, final_labels_out	
 
 if __name__ == '__main__':
 
 	final_data, final_labels = split_data_per_subject()
-	print(len(final_data))
-	print(final_data[39].shape)
+	print(final_data.shape)
+	#print(final_data[39].shape)
+	print('labels')
+	print(final_labels.shape)
+	#print(final_labels[39].shape)
 	
