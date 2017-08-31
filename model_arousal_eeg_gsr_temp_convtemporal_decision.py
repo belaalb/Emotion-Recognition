5d994f593822787f80e565755b8137ddaf91a4a7
Isabela Albuquerque <isabelamcalbuquerque@gmail.com>
2017-08-31 01:01:33 -0400

    Add files via upload

---
+++ model_arousal_eeg_gsr_temp_convtemporal_decision.py
@@ -0,0 +1,148 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class model(nn.Module):
+	
+	def __init__(self):
+
+		super(model, self).__init__()
+
+
+				
+		# EEG network
+		self.conv1_1_eeg = nn.Conv2d(1, 8, (5, 128))			# 384 - 128 + 1 = 257; 32 - 5 + 1 = 28
+		self.conv1_2_eeg = nn.Conv2d(8, 8, (5, 128))			# 257 - 128 + 1 = 130; 28 - 5 + 1 = 24
+		self.conv2_1_eeg = nn.Conv2d(8, 16, (5, 64))			# 130 - 64 + 1 = 67; 24 - 5 + 1 = 20
+		self.conv2_2_eeg = nn.Conv2d(16, 16, (5, 32))			# 67 - 32 + 1 = 36; 20 - 5 + 1 = 16
+		self.conv3_1_eeg = nn.Conv2d(16, 32, (5, 16))			# 36 - 16 + 1 = 21; 16 - 5 + 1 = 12 
+		self.conv3_2_eeg = nn.Conv2d(32, 16, (5, 8))			# 21 - 8 + 1 = 14; 12 - 5 + 1 = 8
+		self.conv4_1_eeg = nn.Conv2d(16, 8, (5, 8))			# 14 - 8 + 1 = 7; 8 - 5 + 1 = 4 
+		self.fc1_eeg = nn.Linear(8*7*4, 60)
+		self.fc2_eeg = nn.Linear(60, 40)					# Representation to be concatenated
+
+		# Output layer
+		self.fc_out_1_eeg = nn.Linear(40, 20)
+		self.fc_out_2_eeg = nn.Linear(20, 10)
+		self.fc_out_eeg = nn.Linear(10, 2)
+		
+
+		# Temp and GSR network
+		self.conv1_1_temp_gsr = nn.Conv1d(2, 16, kernel_size = 192)	# 384 - 192 + 1 = 193
+		self.conv2_1_temp_gsr = nn.Conv1d(16, 32, kernel_size = 128)	# 193 - 128 + 1 = 66
+		self.conv3_1_temp_gsr = nn.Conv1d(32, 16, kernel_size = 64)	# 66 - 64 + 1 = 3
+		self.fc1_temp_gsr = nn.Linear(16*3, 32)
+		self.fc2_temp_gsr = nn.Linear(32, 20)				# Representation to be concatenated
+				
+
+		# Output layer
+		self.fc_out_1_temp_gsr = nn.Linear(20, 10)
+		self.fc_out_2_temp_gsr = nn.Linear(10, 5)
+		self.fc_out_temp_gsr = nn.Linear(5, 2)
+
+
+	def forward_multimodal_arousal(self, x):
+	
+		x_eeg = x[:, 0:32, :]
+		x_eeg = x_eeg.contiguous()
+		x_eeg = x_eeg.view(x_eeg.size()[0], 1, x_eeg.size()[1], x_eeg.size()[2])
+		
+		#x_eog = x[:, 32:34, :]
+		#x_emg = x[:, 34:36, :]
+		
+		#x_resp_ppg = x[:, 37:39, :]
+
+		x_gsr = x[:, 36, :]
+		x_temp = x[:, 39, :]
+
+		x_gsr = x_gsr.contiguous()
+		x_temp = x_temp.contiguous()
+
+		x_gsr = x_gsr.view(x_gsr.size()[0], 1,  x_gsr.size()[1])
+		x_temp = x_temp.view(x_temp.size()[0], 1,  x_temp.size()[1])
+
+				
+
+		x_temp_gsr = torch.cat([x_temp, x_gsr], 1)
+
+
+		#print(x_temp_gsr.size())
+
+
+		#-------EEG
+		x_eeg = self.conv1_1_eeg(x_eeg)
+		x_eeg = F.alpha_dropout(x_eeg, training = self.training)
+		x_eeg = F.selu(x_eeg)
+		x_eeg = self.conv1_2_eeg(x_eeg)
+		x_eeg = F.alpha_dropout(x_eeg, training = self.training)
+		x_eeg = F.selu(x_eeg)
+
+		x_eeg = self.conv2_1_eeg(x_eeg)
+		x_eeg = F.alpha_dropout(x_eeg, training = self.training)
+		x_eeg = F.selu(x_eeg)
+		x_eeg = self.conv2_2_eeg(x_eeg)
+		x_eeg = F.alpha_dropout(x_eeg, training = self.training)
+		x_eeg = F.selu(x_eeg)
+
+		x_eeg = self.conv3_1_eeg(x_eeg)
+		x_eeg = F.alpha_dropout(x_eeg, training = self.training)
+		x_eeg = F.selu(x_eeg)
+		x_eeg = self.conv3_2_eeg(x_eeg)
+		x_eeg = F.alpha_dropout(x_eeg, training = self.training)
+		x_eeg = F.selu(x_eeg)
+
+		x_eeg = self.conv4_1_eeg(x_eeg)
+		x_eeg = F.alpha_dropout(x_eeg, training = self.training)
+		x_eeg = F.selu(x_eeg)
+
+
+		x_eeg = x_eeg.view(x_eeg.size(0), -1)
+		x_eeg = F.selu(self.fc1_eeg(x_eeg))
+		x_eeg = F.alpha_dropout(x_eeg, training = self.training)
+		x_eeg = self.fc2_eeg(x_eeg)
+
+		output_eeg = self.fc_out_1_eeg(x_eeg)
+		output_eeg = F.alpha_dropout(output_eeg, training = self.training)
+		output_eeg = F.selu(output_eeg)
+		output_eeg = self.fc_out_2_eeg(output_eeg)
+		output_eeg = F.alpha_dropout(output_eeg, training = self.training)
+		output_eeg = F.selu(output_eeg)
+		output_eeg = F.softmax(self.fc_out_eeg(output_eeg))
+		
+		
+		#------SKIN TEMP AND GSR
+		x_temp_gsr = self.conv1_1_temp_gsr(x_temp_gsr)
+		x_temp_gsr = F.alpha_dropout(x_temp_gsr, training = self.training)
+		x_temp_gsr = F.selu(x_temp_gsr)
+
+		x_temp_gsr = self.conv2_1_temp_gsr(x_temp_gsr)
+		x_temp_gsr = F.alpha_dropout(x_temp_gsr, training = self.training)
+		x_temp_gsr = F.selu(x_temp_gsr)
+
+		x_temp_gsr = self.conv3_1_temp_gsr(x_temp_gsr)
+		x_temp_gsr = F.alpha_dropout(x_temp_gsr, training = self.training)
+		x_temp_gsr = F.selu(x_temp_gsr)
+				
+		x_temp_gsr = x_temp_gsr.view(x_temp_gsr.size(0), -1)
+		x_temp_gsr = F.selu(self.fc1_temp_gsr(x_temp_gsr))
+		x_temp_gsr = F.alpha_dropout(x_temp_gsr, training = self.training)
+		x_temp_gsr = F.selu(self.fc2_temp_gsr(x_temp_gsr))
+		
+
+		output_temp_gsr = self.fc_out_1_temp_gsr(x_temp_gsr)
+		output_temp_gsr = F.alpha_dropout(output_temp_gsr, training = self.training)
+		output_temp_gsr = F.selu(output_temp_gsr)
+		output_temp_gsr = self.fc_out_2_temp_gsr(output_temp_gsr)
+		output_temp_gsr = F.alpha_dropout(output_temp_gsr, training = self.training)
+		output_temp_gsr = F.selu(output_temp_gsr)
+		output_temp_gsr = F.softmax(self.fc_out_temp_gsr(output_temp_gsr))
+
+
+
+		return output_eeg, output_temp_gsr
+	
+		
+
+		
---
+++ train_loop_decision.py
@@ -0,0 +1,284 @@
+from torch.autograd import Variable
+import torch
+import torch.nn.functional as F
+from DeapDataset import DeapDataset
+from torch.utils.data import DataLoader
+import utils
+
+import numpy as np
+import pickle
+
+import os
+from glob import glob
+from tqdm import tqdm
+
+import gc
+
+from sklearn.metrics import precision_score, f1_score, recall_score
+
+
+class TrainLoop(object):
+
+	def __init__(self, model, optimizer, minibatch_size, checkpoint_path = None, checkpoint_epoch = None, cuda = True):
+		
+		if checkpoint_path is None:
+
+			# Save to current directory
+			self.checkpoint_path = os.getcwd()
+
+		else:
+			self.checkpoint_path = checkpoint_path
+			if not os.path.isdir(self.checkpoint_path):
+
+				os.mkdir(self.checkpoint_path)
+	
+
+		self.save_every_fmt = os.path.join(self.checkpoint_path, 'checkpoint_{}it.pt')
+		self.save_epoch_fmt = os.path.join(self.checkpoint_path, 'checkpoint_{}ep.pt')
+		self.cuda_mode = cuda
+
+
+		if checkpoint_epoch is None:
+			self.model = model
+			self.optimizer = optimizer
+			self.minibatch_size = minibatch_size
+			self.history = {'train_loss': [], 'train_loss_avg': [], 'train_accuracy_avg': [], 'valid_loss': [], 'valid_loss_avg': [], 'valid_accuracy_avg': []}
+			self.total_iters = 0
+			self.cur_epoch = 0
+			self.its_without_improv = 0
+
+		else:
+			self.load_checkpoint(self.save_epoch_fmt.format(checkpoint_epoch))
+
+
+		reciprocal_weights_train, length_train = utils.calculate_weights(step = 'train')
+		self.weight_train = 1 / torch.DoubleTensor(reciprocal_weights_train)
+		self.dataset_train = DeapDataset(step = 'train')
+		self.sampler_train = torch.utils.data.sampler.WeightedRandomSampler(self.weight_train, length_train)
+		self.dataloader_train = DataLoader(self.dataset_train, self.minibatch_size, shuffle = False, sampler = self.sampler_train)
+
+
+		reciprocal_weights_valid, length_valid = utils.calculate_weights(step = 'valid')
+
+		self.weight_valid = 1 / torch.DoubleTensor(reciprocal_weights_valid)
+		self.dataset_valid = DeapDataset(step = 'valid')
+		self.sampler_valid = torch.utils.data.sampler.WeightedRandomSampler(self.weight_valid, length_valid)
+		self.dataloader_valid = DataLoader(self.dataset_valid, self.minibatch_size)
+
+
+
+
+
+	def train(self, n_epochs = 1, patience = 100, n_workers = 1, tl_delay = 1, save_every = None):
+		# Note: Logging expects the losses to be divided by the batch size
+
+		last_val_loss = float('inf')
+
+
+		while (self.cur_epoch < n_epochs) and (self.its_without_improv < patience):
+			train_loss_sum = 0.0
+			train_accuracy = 0.0
+			self.iter_epoch = 0.0
+			
+
+			print('Epoch {}/{}'.format(self.cur_epoch+1, n_epochs))
+			train_iter = tqdm(enumerate(self.dataloader_train))
+
+			print(train_iter)
+
+			print(self.cur_epoch)
+
+			for t, batch in train_iter:
+
+				loss, acc = self.train_step(batch)
+
+				self.history['train_loss'].append(loss)	
+				
+				train_loss_sum += loss
+				self.total_iters += 1
+				self.iter_epoch += 1
+				
+				train_accuracy += acc
+
+				print(acc)
+				
+				if save_every is not None:
+					if self.total_iters % save_every == 0:
+						torch.save(self, self.save_every_fmt.format(self.total_iters))
+
+				
+			
+			train_loss_avg = train_loss_sum / self.iter_epoch  
+			print('Training loss: {}'.format(train_loss_avg))
+			self.history['train_loss_avg'].append(train_loss_avg)
+
+			train_accuracy_avg = train_accuracy / self.iter_epoch  
+			print('Training accuracy: {}'.format(train_accuracy_avg))
+			self.history['train_accuracy_avg'].append(train_accuracy_avg)
+
+			# Validation
+			valid_loss_sum = 0.0
+			n_valid_iterations = 0
+			valid_accuracy = 0.0
+
+			for t, batch in enumerate(self.dataloader_train):
+				
+				loss, acc = self.valid(batch)
+				
+				self.history['valid_loss'].append(loss)
+
+				valid_loss_sum += loss
+				n_valid_iterations += 1
+
+				valid_accuracy += acc	
+
+				print(acc)
+
+			valid_loss_avg = valid_loss_sum / n_valid_iterations	
+			print('Validation loss: {}'.format(valid_loss_avg))
+			self.history['valid_loss_avg'].append(valid_loss_avg)
+
+			valid_accuracy_avg = valid_accuracy / n_valid_iterations 	
+			print('Validation accuracy: {}'.format(valid_accuracy_avg))
+			self.history['valid_accuracy_avg'].append(valid_accuracy_avg)
+
+			self.checkpointing()
+
+			self.cur_epoch += 1
+
+			if valid_loss_avg < last_val_loss:
+				self.its_without_improv = 0
+
+			else:
+				self.its_without_improv += 1
+
+			last_val_loss = valid_loss_avg	
+
+		# saving final models
+		print('Saving final model...')
+
+		torch.save(self.model.state_dict(), './model1.pt')
+
+	def train_step(self, batch):
+
+		self.model.train()
+			
+		x = batch['data']
+		y = batch['label']
+		
+		print('\n')
+
+		x = Variable(x)
+		y = Variable(y, requires_grad = False)
+
+		if self.cuda_mode:
+			x = x.cuda()
+			y = y.cuda()
+
+		out_eeg, out_temp_gsr = self.model.forward_multimodal_arousal(x)
+
+		targets = y[:, 1].contiguous()
+		targets = targets.view(targets.size(0), 1)
+
+
+		loss_eeg = F.cross_entropy(out_eeg, y[:, 1])
+		loss_temp_gsr = F.cross_entropy(out_temp_gsr, y[:, 1])
+
+		loss_fusion = loss_eeg + loss_temp_gsr
+
+		self.optimizer.zero_grad()
+		loss_fusion.backward()
+		self.optimizer.step()
+
+		loss_return = torch.sum(loss_eeg.data) + torch.sum(loss_temp_gsr.data)
+		
+		out_fusion = (out_eeg + out_temp_gsr) / 2
+
+		out_fusion_max = (torch.max(out_fusion, 1)[1])
+
+		accuracy = torch.mean((out_fusion_max == y[:, 1]).float())
+		accuracy_return = accuracy.data
+
+
+		if (self.iter_epoch % 5 == 0):
+
+			out_fusion_max = out_fusion_max.cpu().data.numpy()
+			targets = targets.cpu().data.numpy()
+
+			print('Precision')
+			print(precision_score(targets, out_fusion_max))
+			print('Recall')
+			print(recall_score(targets, out_fusion_max))
+			print('F1 score')
+			print(f1_score(targets, out_fusion_max))
+			print(out_fusion_max)
+ 	
+
+		return loss_return, accuracy_return
+
+
+	def valid(self, batch):
+
+		self.model.eval()
+		
+		x = batch['data']
+		y = batch['label']
+
+		x = Variable(x, requires_grad = False)
+		y = Variable(y, requires_grad = False)
+
+		if self.cuda_mode:
+			x = x.cuda()
+			y = y.cuda()
+			
+
+		out_eeg, out_temp_gsr = self.model.forward_multimodal_arousal(x)
+
+		loss_eeg = F.cross_entropy(out_eeg, y[:, 1])
+		loss_temp_gsr = F.cross_entropy(out_eeg, y[:, 1])
+
+		loss_return = torch.sum(loss_eeg.data) + torch.sum(loss_temp_gsr.data)	# If sum receives: i) Tensor (loss.data), it returns a float; ii) Variable (loss), it returns a tensor
+
+		out_fusion = (out_eeg + out_temp_gsr) / 2
+
+		out_fusion_max = (torch.max(out_fusion, 1)[1])
+
+
+		accuracy = torch.mean((out_fusion_max == y[:, 1]).float())
+		accuracy_return = accuracy.data
+
+		return loss_return, accuracy_return
+
+	def checkpointing(self):
+		
+		# Checkpointing
+		print('Checkpointing...')
+		ckpt = {'model_state': self.model.state_dict(),
+		'optimizer_state': self.optimizer.state_dict(),
+		'history': self.history,
+		'total_iters': self.total_iters,
+		'cur_epoch': self.cur_epoch,
+		'its_without_improve': self.its_without_improv}
+		torch.save(ckpt, self.save_epoch_fmt.format(self.cur_epoch))
+
+	def load_checkpoint(self, ckpt):
+
+		if os.path.isfile(ckpt):
+
+			ckpt = torch.load(ckpt)
+			
+			# Load model state
+			self.model.load_state_dict(ckpt['model_state'])
+			
+			# Load optimizer state
+			self.optimizer.load_state_dict(ckpt['optimizer_state'])
+			
+			# Load history
+			self.history = ckpt['history']
+			self.total_iters = ckpt['total_iters']
+			self.cur_epoch = ckpt['cur_epoch']
+			self.its_without_improv = ckpt['its_without_improve']
+
+		else:
+
+			print('No checkpoint found at {}'.format(ckpt))				
